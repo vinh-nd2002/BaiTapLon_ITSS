ffaa6a6f28936994e33e60d571ddf2b5712abe20
vinh-nd2002 <nguduyvinh2k2@gmail.com>
2023-06-26 01:00:39 +0700

    add pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Login, Home, Public } from "./pages/public";
+import {
+  Login,
+  Home,
+  Public,
+  Blogs,
+  FAQs,
+  DetailProduct,
+  Services,
+  Products,
+} from "./pages/public";
 import path from "./utils/path";
 import { useDispatch } from "react-redux";
 import { getCategories } from "./stores/asyncAction";
@@ -15,8 +24,16 @@ function App() {
       <Routes>
         <Route path={path.PUBLIC} element={<Public />}>
           <Route path={path.HOME} element={<Home />}></Route>
-          <Route path={path.LOGIN} element={<Login />}></Route>
+          <Route path={path.BLOGS} element={<Blogs />}></Route>
+          <Route path={path.FAQS} element={<FAQs />}></Route>
+          <Route
+            path={path.DETAIL_PRODUCT__PID__TITLE}
+            element={<DetailProduct />}
+          ></Route>
+          <Route path={path.OUR_SERVICES} element={<Services />}></Route>
+          <Route path={path.PRODUCTS} element={<Products />}></Route>
         </Route>
+        <Route path={path.LOGIN} element={<Login />}></Route>
       </Routes>
     </div>
   );
